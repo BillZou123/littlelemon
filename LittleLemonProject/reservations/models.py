f1f63cb9c5f5f00fe750ae7e5f086b0480a1b483
BillZou <1072387567@qq.com>
2024-05-20 00:07:12 -0400

    add 2 models:Booking, Menu

--- LittleLemonProject/reservations/models.py
+++ LittleLemonProject/reservations/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    num_guest = models.IntegerField()
+    booking_date  = models.DateField()
+
+    def __str__(self):
+        return self.name +"'s booking at "+ str(self.booking_date)
+    
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
